a1dc77b44846a7a297d086555b5094c403b04953
Duffew <pbsvimqx@students.codeinstitute.net>
2025-02-18 08:41:11 +0000

    feat: add review_detail.html and view, add default image

--- review/urls.py
+++ review/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     # Define a urlpattern for ReviewList with the name 'home'
     # Map the root URL to the ReviewList view, naming it 'home' 
     path('', views.ReviewList.as_view(), name='home'),
+    # Define a urlpattern for review_detail with the name 'review_detail'
+    # This pattern will enable the slug for each reveiw to become part of
+    # the URL
+    path('<slug:slug>/', views.review_detail, name="review_detail"),
 ]
--- review/views.py
+++ review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Review
 
@@ -11,3 +11,39 @@ class ReviewList(generic.ListView):
     # Specify the html template to be used for rendering the view
     template_name = "review/index.html"
     paginate_by = 6
+
+
+# Define the view function for displaying the details of a single review with 
+# the following parameters:
+# - request: The current HttpRequest object containing all the information 
+# about the client's request
+# - slug: A unique identifier used to retrieve the specific review object 
+# from the database's Review model
+def review_detail(request, slug):
+    """
+    Display an individual :model:`review.Review`.
+
+    **Context**
+
+    ``review``
+        An instance of :model:`review.Review`.
+
+    **Template:**
+
+    :template:`review/review_detail.html`
+    """
+
+    # Create a QuerySet of Review objects that have a status of 1 (published)
+    queryset = Review.objects.filter(status=1)
+    # Get a single Review object from the QuerySet using the slug, 
+    # or return a 404 error if the object does not exist
+    review = get_object_or_404(queryset, slug=slug)
+
+    # Render the 'review_detail.html' template with the given context
+    # The context dictionary contains a single key 'review' which maps to 
+    # the review object 
+    return render(
+        request,  # The current HttpRequest object
+        "review/review_detail.html",  # The path to the template to be rendered
+        {"review": review},  # Context dictionary containing the review object
+    )
